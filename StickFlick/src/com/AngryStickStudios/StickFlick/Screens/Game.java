e76a8d8f67e2f927c4841622bf4bb47653865dca
comfyfutons <sethpaskert@Seths-MacBook-Pro.local>
2014-02-01 03:09:05 -0500

    Initial Commit (Real)
    after playing with github here is the full project

---
+++ StickFlick/src/com/AngryStickStudios/StickFlick/Screens/Game.java
@@ -0,0 +1,47 @@
+package com.AngryStickStudios.StickFlick.Screens;
+
+import com.badlogic.gdx.Screen;
+import com.AngryStickStudios.StickFlick.StickFlick;
+
+public class Game implements Screen{
+
+	public Game(StickFlick game){
+		
+	}
+	
+	@Override
+	public void render(float delta) {
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		
+	}
+
+	@Override
+	public void show() {
+		
+	}
+
+	@Override
+	public void hide() {
+		
+	}
+
+	@Override
+	public void pause() {
+		
+	}
+
+	@Override
+	public void resume() {
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+
+}
