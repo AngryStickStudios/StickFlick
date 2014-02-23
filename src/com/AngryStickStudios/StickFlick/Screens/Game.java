7155c3356a1c6d258cf7dc04dc3bf77af5779d71
Andres Hernandez <andresfhernandez@ufl.edu>
2014-02-23 01:04:57 -0500

    Timer and main menu button from pause menu
    Time is now displayed during game play (mm:ss). Main menu is now
    accessible from the pause menu.

--- src/com/AngryStickStudios/StickFlick/Screens/Game.java
+++ src/com/AngryStickStudios/StickFlick/Screens/Game.java
@@ -5,6 +5,7 @@ package com.AngryStickStudios.StickFlick.Screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -17,19 +18,27 @@ import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
+import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.utils.Timer.Task;
 import com.AngryStickStudios.StickFlick.StickFlick;
 import com.AngryStickStudios.StickFlick.Controller.GestureDetection;
 import com.AngryStickStudios.StickFlick.Entities.WalkingEnemy;
 
-public class Game implements Screen{
+public class Game implements Screen {
 
 	public static final int GAME_RUNNING = 1;
     public static final int GAME_PAUSED = 0;
     private int gameStatus = 1;
+    private float timeTrack = 0;
+    private int seconds = 0;
+    private int minutes = 0;
+    private String formattedTime = "0:00";
 	
 	StickFlick game;
 	SpriteBatch batch;
@@ -40,11 +49,14 @@ public class Game implements Screen{
 	GestureDetector gd;
 	TextureAtlas atlas;
 	InputMultiplexer im;
-	TextButton pauseButton, resumeButton;
+	TextButton pauseButton, resumeButton, mainMenuButton;
 	WalkingEnemy testEnemy;
+	LabelStyle labelStyle;
+	Label timer;
 	
 	public Game(StickFlick game){
 		this.game = game;
+		
 		testEnemy = new WalkingEnemy("basic", 100, Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
 	}
 	
@@ -56,7 +68,30 @@ public class Game implements Screen{
 		if (gameStatus == 1) {
 			stage.act(Gdx.graphics.getDeltaTime());
 			batch.begin();
-			stage.draw();
+			stage.draw();	
+			
+			timeTrack += Gdx.graphics.getDeltaTime();
+			if (timeTrack >= 1f) {
+				
+				timeTrack = timeTrack - 1f;
+				seconds++;
+				
+				if (seconds >= 60) {
+					seconds = seconds - 60;
+					minutes++;
+				}
+				
+				if (seconds < 10) {
+					formattedTime = Integer.toString(minutes) + ":0" + Integer.toString(seconds);
+				}
+				
+				else {
+					formattedTime = Integer.toString(minutes) + ":" + Integer.toString(seconds);
+				}
+				
+				timer.setText(formattedTime);
+			}
+			
 			batch.end();
 		}
 		
@@ -105,6 +140,13 @@ public class Game implements Screen{
 		pauseButton.setY(Gdx.graphics.getHeight() * 0.90f);
 		stage.addActor(pauseButton);
 		
+		labelStyle = new LabelStyle(white, Color.BLACK);
+		timer = new Label(formattedTime, labelStyle);
+		timer.setHeight(Gdx.graphics.getHeight() / 24);
+		timer.setX(Gdx.graphics.getWidth() * 0.025f);
+		timer.setY(Gdx.graphics.getHeight() * 0.95f);
+		stage.addActor(timer);
+	
 		//Pause button stage, not main stage
 		resumeButton = new TextButton("Resume", buttonStyle);
 		resumeButton.setWidth(Gdx.graphics.getWidth() / 6);
@@ -112,6 +154,13 @@ public class Game implements Screen{
 		resumeButton.setX(Gdx.graphics.getWidth()/2 - resumeButton.getWidth()/2);
 		resumeButton.setY(Gdx.graphics.getHeight()/2 - resumeButton.getHeight()/2);
 		pauseStage.addActor(resumeButton);
+		
+		mainMenuButton = new TextButton("Main Menu", buttonStyle);
+		mainMenuButton.setWidth(Gdx.graphics.getWidth() / 6);
+		mainMenuButton.setHeight(Gdx.graphics.getHeight() / 12);
+		mainMenuButton.setX(Gdx.graphics.getWidth()/2 - resumeButton.getWidth()/2);
+		mainMenuButton.setY(Gdx.graphics.getHeight()/2 - resumeButton.getHeight()*2);
+		pauseStage.addActor(mainMenuButton);
 		
 		stage.addActor(testEnemy.getImage());
 		testEnemy.getImage().addCaptureListener(new ActorGestureListener());
@@ -148,6 +197,23 @@ public class Game implements Screen{
 					@Override
 					public void run() {
 						resumeGame();
+					}
+				})));
+			}
+		});
+		
+		mainMenuButton.addListener(new InputListener(){
+			public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+				System.out.println("down");
+				return true;
+			}
+			public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+				System.out.println("up");
+				
+				pauseStage.addAction(Actions.sequence(Actions.fadeOut(.3f), Actions.run(new Runnable() {
+					@Override
+					public void run() {
+						((StickFlick) Gdx.app.getApplicationListener()).setScreen(new MainMenu(game));
 					}
 				})));
 			}
