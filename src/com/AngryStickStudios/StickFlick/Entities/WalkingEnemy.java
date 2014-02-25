353eb5ba918f9bd2a53239fe2da8aa48974804da
comfyfutons <jgjdmjtwm@gmail.com>
2014-02-25 18:01:07 -0500

    Changed to use getPosition method everywhere in walking enemy

--- src/com/AngryStickStudios/StickFlick/Entities/WalkingEnemy.java
+++ src/com/AngryStickStudios/StickFlick/Entities/WalkingEnemy.java
@@ -68,16 +68,24 @@ public class WalkingEnemy extends Entity {
 	}
 	
 	public void FindDestOnWall() {
-		Vector2 straightDown = new Vector2(enemy.getX(), 20);
+		Vector2 straightDown = getPosition();
+		straightDown.y = Gdx.graphics.getHeight() * 0.04f;
+		
 		int adjAmt = (int) Math.round(((Math.random() * (Gdx.graphics.getWidth() / 5)) - (Gdx.graphics.getWidth() / 10)));
 		System.out.println(adjAmt);
 		straightDown.x += adjAmt;
+		if(straightDown.x > Gdx.graphics.getWidth() * 0.98f){
+			straightDown.x = Gdx.graphics.getWidth() * 0.98f;
+		}
+		if(straightDown.x < Gdx.graphics.getWidth() * 0.02f){
+			straightDown.x = Gdx.graphics.getWidth() * 0.02f;
+		}
 		destination = straightDown;
 	}
 
 	public void pickedUp() {
 		held = true;
-		lastPos = new Vector2(enemy.getX(), enemy.getY());
+		lastPos = new Vector2(getPosition().x, getPosition().y);
 	}
 
 	public void Released(Vector2 speed) {
@@ -94,7 +102,7 @@ public class WalkingEnemy extends Entity {
 			shadow.setX(enemy.getX());
 			shadow.setY(lastPos.y - ((enemy.getHeight() / 8) * scale));
 			
-			if(enemy.getY() <= lastPos.y)
+			if(getPosition().y <= lastPos.y)
 			{
 				setPosition(Gdx.input.getX(), lastPos.y + ((enemy.getHeight() / 2) * scale));
 			}
@@ -104,26 +112,23 @@ public class WalkingEnemy extends Entity {
 		if(floating)
 		{
 			Vector2 newPos = new Vector2(0,0);
-			newPos.x = enemy.getX() + flySpeed.x;
+			newPos.x = getPosition().x + flySpeed.x;
 			
 			if(newPos.x < 8) newPos.x = 8;
 			if(newPos.x > Gdx.graphics.getWidth() - 16) newPos.x = Gdx.graphics.getWidth() - 16;
 
-			if(lastPos.y >= enemy.getY() + flySpeed.y)
+			if(lastPos.y >= getPosition().y + flySpeed.y)
 			{
 				newPos.y = lastPos.y;
 				floating = false;
-				//setPosition(newPos.x, newPos.y);
-				enemy.setX(newPos.x);
-				enemy.setY(newPos.y);
+				setPosition(newPos.x, newPos.y);
+
 				FindDestOnWall();
 			}
 			else
 			{
-				newPos.y = enemy.getY() + flySpeed.y;
-				//setPosition(newPos.x, newPos.y);
-				enemy.setX(newPos.x);
-				enemy.setY(newPos.y);
+				newPos.y = getPosition().y + flySpeed.y;
+				setPosition(newPos.x, newPos.y);
 			}
 
 			flySpeed.y -= 1;
@@ -133,17 +138,22 @@ public class WalkingEnemy extends Entity {
 		}
 		
 		//walk and shit
-		Vector2 compVec = new Vector2(destination.x - enemy.getX(), destination.y - enemy.getY());
-		Vector2 normVec = compVec.nor();
-		Vector2 walkVec = normVec.scl(50 * delta);
-		
-		//System.out.println("destination = " + destination.x + " " + destination.y);
-		enemy.setX(enemy.getX() + walkVec.x);
-		enemy.setY(enemy.getY() + walkVec.y);
-		
-		if(enemy.getY() < 20) enemy.setY(20);
-		
-		shadow.setX(enemy.getX());
-		shadow.setY(enemy.getY() - ((enemy.getHeight() / 8) * scale));
+		if(getPosition().y > Gdx.graphics.getHeight() * 0.05f){
+			Vector2 compVec = new Vector2(destination.x - getPosition().x, destination.y - getPosition().y);
+			Vector2 normVec = compVec.nor();
+			Vector2 walkVec = normVec.scl(50 * delta);
+
+
+			//System.out.println("destination = " + destination.x + " " + destination.y);
+			setPosition(getPosition().x + walkVec.x, getPosition().y + walkVec.y);
+
+
+			if(getPosition().y < 20){
+				setPosition(getPosition().x, 20);
+			}
+
+			shadow.setX(enemy.getX());
+			shadow.setY(enemy.getY() - ((enemy.getHeight() / 8) * scale));
+		}
 	}
 }
