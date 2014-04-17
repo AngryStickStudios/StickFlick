6e5c8e404d1903f6b999bd66c4184548d3154e4f
Irene <freirevi@gmail.com>
2014-04-17 17:13:37 -0400

    Boiling Oil Animation
    added the boiling oil animation class

---
+++ src/com/AngryStickStudios/StickFlick/Entities/BoilingOil.java
@@ -0,0 +1,76 @@
+package com.AngryStickStudios.StickFlick.Entities;
+ 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.AngryStickStudios.StickFlick.StickFlick;
+import com.AngryStickStudios.StickFlick.Controller.AnimationLoader;
+ 
+public class BoilingOil extends Entity {
+        float scale, mscale;
+        
+        private Animation currentanim;
+        private TextureRegion currentframe;
+        private TextureRegionDrawable powerUpDrawable;
+        float animationStateTime;
+       
+        Image bOil;
+ 
+        public BoilingOil(AnimationLoader anims, float posX, float posY){
+                super("boiling_oil", 0, anims);
+                scale = 0.5f;
+                mscale = 1f;
+                
+                animationStateTime = 0;
+                currentanim = anims.getAnim("boilingOil");
+                currentframe = currentanim.getKeyFrame(animationStateTime, true);
+                powerUpDrawable = new TextureRegionDrawable(currentframe);
+               
+                // Create enemy Image/Actor
+                bOil = new Image(powerUpDrawable);
+                bOil.setX(posX);
+                bOil.setY(posY);
+                bOil.setScale(scale);
+        
+        }
+       
+        
+       
+        public Image getImage(){
+                return bOil;
+        }
+       
+        public void setPosition(float x, float y){
+                bOil.setX(x - ((bOil.getWidth() / 2) * (scale / 2)));
+                bOil.setY(y - ((bOil.getHeight() / 2) * scale));
+        }
+       
+        public Vector2 getPosition()
+        {
+                return new Vector2(bOil.getX() + ((bOil.getWidth() / 2) * (scale / 2)), bOil.getY() + ((bOil.getHeight() / 2) * scale));
+        }
+     
+       
+        public Vector2 getSize(){
+                return new Vector2(bOil.getWidth() * (scale / 2), bOil.getHeight() * scale);
+        }
+       
+        
+       
+        public void Anim(float delta)
+        {
+                
+                        currentframe = currentanim.getKeyFrame(animationStateTime += delta, false);
+                        powerUpDrawable.setRegion(currentframe);
+                        bOil.setDrawable(powerUpDrawable);
+                
+               
+        }
+}
